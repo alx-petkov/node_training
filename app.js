c40a56507337a570220c08b3f558ce80f92394ef
DESKTOP-DHIUPI3\ThinkCentre <alx.petkov@gmail.com>
2018-04-22 16:58:16 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// code for testing es6 functionality
+// class Polygon {
+//     constructor(height, width) {
+//         this.area = height * width;
+//     }
+// }
+//
+// console.log(new Polygon(4,3).area);
+// // expected output: 12
+//
+//
+// const arrFunc = () => {
+//     let abrakadabra = 'abrakadabra';
+//     console.log(abrakadabra)
+// }
+//
+// arrFunc();
+
+const config = require('./config/config.json');
+console.log(config.name);
+
+// onst UserClass = require('./models/User.js');
+// onst ProductClass = require('./models/Product.js');
