d3895a73f99474bd3803ad15a287bcd0e616e0b9
DESKTOP-DHIUPI3\ThinkCentre <alx.petkov@gmail.com>
2018-05-22 00:29:44 +0300

    all actions implemented - no validation

--- utils/streams.js
+++ utils/streams.js
@@ -16,46 +16,97 @@ let showHelp = ['h', 'help'].indexOf(propsKeys[1]) + 1; // 2.d
 
 
 var actions = {
+    // node ./utils/streams.js -a reverse
     reverse: function(str) {
         process.stdin.on('data', function(data){
             const inputStr = data.toString().trim();
             process.stdout.write(inputStr.split("").reverse().join("") + "\n");
         })
     },
+    // node ./utils/streams.js -a transform
     transform: function(str) { 
         process.stdin.on('data', function(data){
             const inputStr = data.toString().trim();
             process.stdout.write(inputStr.toUpperCase() + "\n");
         })
     },
+    // node ./utils/streams.js -a outputFile -f ../data/file_1.csv
     outputFile: function(filePath) { 
         outputStream = fs.createReadStream(__dirname + "/" + filePath, 'utf8');
         outputStream.pipe(process.stdout);
     },
-    convertFromFile: function(filePath) { 
-       fs.createReadStream(__dirname + "/" + filePath, 'utf8', (err, content) => {
-            if(err){ 
-               console.error(err); 
-            } else {
-                csv()
-                .fromString(content)
-                .on('json',(jsonLine)=>{ //parsing finished
-                        process.stdout.write(jsonLine);
-                })
-            }
+    // node ./utils/streams.js -a convertFromFile -f ../data/file_1.csv
+    convertFromFile: function(filePath) {
+        let readStream = fs.createReadStream(__dirname + "/" + filePath, 'utf8');
+        // readStream.pipe(process.stdout);
+        let chunks = [];
+
+
+        // Handle any errors while reading
+        readStream.on('error', err => {
+            // handle error
+            console.error(err);
+        });
+
+        // Listen for data
+        readStream.on('data', chunk => {
+            csv()
+            .fromString(chunk.trim())
+            .on('json',(jsonLine)=>{ //parsing finished
+                chunks.push(jsonLine);
+            })
+        });
+
+        // File is done being read
+        readStream.on('close', () => {
+            // Output the converted data
+            process.stdout.write(JSON.stringify(chunks));
         });
     },
-    convertToFile: function(filePath) { console.log('convertToFile') }
+    // node ./utils/streams.js -a convertToFile -f ../data/file_1.csv
+    convertToFile: function(filePath) {
+        /*let readStream = fs.createReadStream(__dirname + "/" + filePath, 'utf8');
+        // readStream.pipe(process.stdout);
+        let chunks = [];
+
+
+        // Handle any errors while reading
+        readStream.on('error', err => {
+            // handle error
+            console.error(err);
+        });
+
+        // Listen for data
+        readStream.on('data', chunk => {
+            csv()
+                .fromString(chunk.trim())
+                .on('json',(jsonLine)=>{ //parsing finished
+                    chunks.push(jsonLine);
+                })
+        });
+
+        // File is done being read
+        readStream.on('close', () => {
+            // Output the converted data
+            process.stdout.write(JSON.stringify(chunks));
+        });*/
+        console.log('wow');
+        fs.createReadStream(__dirname + "/" + filePath, 'utf8')
+            .pipe(csv())
+            .pipe(fs.createWriteStream(__dirname + "/" + filePath.replace('csv', 'json')));
+    }
    
 };
 
 
 if(propsKeys.length <= 1){
+    // node ./utils/streams.js
     console.log('no argemunets passed');
     showHelp = true;
 }
 
 if (showHelp){
+    // node ./utils/streams.js -h
     console.log('this is useless help message');
     return;
 };
