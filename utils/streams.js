d38c3249b8ee7a8e4c672318ffab74564fc938d9
Alexander_Petkov <Alexander_Petkov@epam.com>
2018-05-21 17:45:38 +0300

    adding reverse and transform actions

--- utils/streams.js
+++ utils/streams.js
@@ -1,28 +1,49 @@
 
-var comands = require('minimist')(process.argv.slice(2));
+const propsObj = require('minimist')(process.argv.slice(2));
+const propsKeys = Object.keys(propsObj);
+let showHelp = ['h', 'help'].indexOf(propsKeys[1]) + 1; // 2.d
 
-const passedKeys = Object.keys(comands);
+/* var args = require('minimist')(process.argv.slice(2), {
+  alias: { h: 'help', a: 'action', f: 'file' },
+  unknown: function (arg) {
+      if (arg){
+          console.log('you passed an unexpected argument');
+      }
+   } // invoked on unknown param 
+}) */
 
-let showHelp = ['h', 'help'].indexOf(passedKeys[1]) + 1;
+
+var actions = {
+    reverse: function(str) {
+        process.stdin.on('data', function(data){
+            const inputStr = data.toString().trim();
+            process.stdout.write(inputStr.split("").reverse().join("") + "\n");
+        })
+    },
+    transform: function(str) { 
+        process.stdin.on('data', function(data){
+            const inputStr = data.toString().trim();
+            process.stdout.write(inputStr.toUpperCase() + "\n");
+        })
+    },
+    outputFile: function(filePath) { console.log('otputFile')},
+    convertFromFile: function(filePath) { console.log('convertFromFile') },
+    convertToFile: function(filePath) { console.log('convertToFile') }
+   
+};
 
 
-console.log(comands, passedKeys, 'hello', showHelp);
-
-
-
-// Main actions to be called
-
-function reverse(str) { console.log('reverse')};
-function transform(str) { console.log('transform')};
-function outputFile(filePath) { console.log('otputFile')};
-function convertFromFile(filePath) { console.log('convertFromFile') };
-function convertToFile(filePath) { console.log('convertToFile') };
-
-if(passedKeys.length <= 1){
-    console.log('wrong input');
+if(propsKeys.length <= 1){
+    console.log('no argemunets passed');
     showHelp = true;
 }
 
 if (showHelp){
-    console.log('help usage message');
+    console.log('this is useless help message');
+    return;
 };
+
+const actionName = propsObj['a'] ? propsObj['a'] : propsObj['action'];
+
+actions[actionName]();
+
