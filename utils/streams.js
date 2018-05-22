4c2c43a46b4cad492bc003d9c45f054ce7ddbe6c
Alexander_Petkov <Alexander_Petkov@epam.com>
2018-05-22 13:17:15 +0300

    adding cssBlunder no end request

--- utils/streams.js
+++ utils/streams.js
@@ -6,7 +6,7 @@ const propsKeys = Object.keys(propsObj);
 let showHelp = ['h', 'help'].indexOf(propsKeys[1]) + 1; // 2.d
 
 /* var args = require('minimist')(process.argv.slice(2), {
-  alias: { h: 'help', a: 'action', f: 'file' },
+  alias: { h: 'help', a: 'action', f: 'file', p: 'path' },
   unknown: function (arg) {
       if (arg){
           console.log('you passed an unexpected argument');
@@ -65,35 +65,63 @@ var actions = {
     },
     // node ./utils/streams.js -a convertToFile -f ../data/file_1.csv
     convertToFile: function(filePath) {
-        /*let readStream = fs.createReadStream(__dirname + "/" + filePath, 'utf8');
-        // readStream.pipe(process.stdout);
-        let chunks = [];
-
-
-        // Handle any errors while reading
-        readStream.on('error', err => {
-            // handle error
-            console.error(err);
-        });
-
-        // Listen for data
-        readStream.on('data', chunk => {
-            csv()
-                .fromString(chunk.trim())
-                .on('json',(jsonLine)=>{ //parsing finished
-                    chunks.push(jsonLine);
-                })
-        });
-
-        // File is done being read
-        readStream.on('close', () => {
-            // Output the converted data
-            process.stdout.write(JSON.stringify(chunks));
-        });*/
-        console.log('wow');
         fs.createReadStream(__dirname + "/" + filePath, 'utf8')
             .pipe(csv())
             .pipe(fs.createWriteStream(__dirname + "/" + filePath.replace('csv', 'json')));
+    },
+    // node ./utils/streams.js -a cssBlunder -p ../data/file_1.csv
+    cssBlunder: function(dirPath){
+
+        var fileNames = fs.readdirSync(__dirname + "/" + dirPath);
+        var wstream = fs.createWriteStream(__dirname + "/" + dirPath + '/bundle.css');
+        let chunks = [];
+        let test = 0;
+
+        fileNames.forEach(function(file){
+                let readStream = fs.createReadStream(__dirname + "/" + dirPath + '/' + file, 'utf8');
+                   
+                // Handle any errors while reading
+                readStream.on('error', err => {
+                    // handle error
+                    console.error(err);
+                 });
+
+                // Listen for data
+                readStream.on('data', chunk => {
+                    chunks.push(chunk.trim());
+                    wstream.write(chunk);
+                });
+
+                // File is done being read
+                readStream.on('close', () => {
+                    test = 5;
+                    // Output the converted data
+                    // process.stdout.write(JSON.stringify(chunks));
+                    wstream.write('\n\r end of file' + file + '======== \n\r');
+                });
+        })
+        console.log(chunks, test);
+
+        /*fs.readdir(__dirname + "/" + dirPath, function(err, filenames) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log(filenames);
+            filenames.forEach(function(filename) {
+                fs.readFile(__dirname + "/" + dirPath + filename, 'utf-8', function(err, content) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    // onFileContent(filename, content);
+                    console.log(filename, content);
+                    fs.createWriteStream(__dirname + "/" + dirPath + 'bundle.css');
+                });
+            });
+        });*/
+
+
     }
    
 };
@@ -113,6 +141,10 @@ if (showHelp){
 
 const actionName = propsObj['a'] ? propsObj['a'] : propsObj['action'];
 const filePath = propsObj['f'] ? propsObj['f'] : propsObj['file'];
+const dirPath = propsObj['p'] ? propsObj['p'] : propsObj['path'];
 
-actions[actionName](filePath);
-
+if(actionName == 'cssBlunder'){
+    actions[actionName](dirPath); 
+} else{
+    actions[actionName](filePath);
+}
