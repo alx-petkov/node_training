6f22903226e86e33d661e04090f6414d57e8f72c
Alexander_Petkov <Alexander_Petkov@epam.com>
2018-06-01 17:15:47 +0300

    appending web data to bundle.css

--- utils/streams.js
+++ utils/streams.js
@@ -91,100 +91,61 @@ var actions = {
 
     // node ./utils/streams.js -a cssBlunder -p ../css/
     cssBlunder: function(dirPath){
+        const localDir = __dirname + "/" + dirPath;
+        const bundleLocation = __dirname + "/" + dirPath + '/bundle.css';
+        const webFileLocation = 'https://drive.google.com/uc?export=download&id=1tCm9Xb4mok4Egy2WjGqdYYkrGia0eh7X';
+        const fileNames = fs.readdirSync(localDir);
+        const wstream = fs.createWriteStream(bundleLocation);
+        let finalData = '';
+        let transferCount = 0;
 
-        var fileNames = fs.readdirSync(__dirname + "/" + dirPath);
-        var wstream = fs.createWriteStream(__dirname + "/" + dirPath + '/bundle.css')
+
+        const downloadFile = (url) => {
+            return new Promise((resolve, reject) => {
+                https.get(url, (res) => {
+                    if(res.headers.location) {
+                        return downloadFile(res.headers.location).then(stream => resolve(stream));
+                    }
+                    resolve(res);
+                });
+            });
+        };
 
 
         fileNames.forEach(function(file){
-                let readStream = fs.createReadStream(__dirname + "/" + dirPath + '/' + file, 'utf8');
-                   
-                // Handle any errors while reading
-                readStream.on('error', err => {
-                    console.error(err);
-                 });
+                let readStream = fs.createReadStream(localDir + '/' + file, 'utf8');
 
-                // Listen for data
-                readStream.on('data', chunk => {
-                    wstream.write(chunk);
-                });
-
-                // File is done being read
-                readStream.on('end', () => {
-
-                    if (file == fileNames[fileNames.length - 1]){
-                        const adress = 'https://drive.google.com/file/d/1tCm9Xb4mok4Egy2WjGqdYYkrGia0eh7X/edit';
-                        const adress2 = 'https://epa.ms/nodejs18-hw3-css';
-                        const adress3 = 'https://www.googleapis.com/drive/v2/files/1tCm9Xb4mok4Egy2WjGqdYYkrGia0eh7X/edit';
-
-                        var options = {
-                            hostname: 'drive.google.com',
-                            // hostname: 'en.wikipedia.org',
-                            port: 443,
-                            path: '/uc?export=download&id=1tCm9Xb4mok4Egy2WjGqdYYkrGia0eh7X',
-                            // path: '/wiki/Donald_Glover',
-                            method: "GET"
-                        }
-
-
-                        /*var req = https.request(options, function (response) {
-                            var body = '';
-                            var i = 0;
-                            response.setEncoding('utf8');
-                            response.on('data', function (htchunk) {
-                                i++;
-                                body += htchunk;
-                                console.log('BODY Part: ' + i, htchunk);
-                            });
-                            response.on('end', function () {
-
-                                    console.log(body);
-                                    console.log('Finished');
-                            });
-                        });
-
-                        req.end();*/
+                readStream.pipe(wstream)
+                
+                wstream.on('finish', () => { 
+                    transferCount++;
+                    if(transferCount === fileNames.length){
+                        console.log('all done reading local files');
+                        // downloadFile(webFileLocation)
+                        // .then(stream => stream.pipe(wstream));  // trows error
                     }
-                });
+                } );
+
         })
 
 
-        // =========================================================
+        wstream.on('finish', () => {
+            downloadFile(webFileLocation)
+            .then(stream => stream.on('data', (data) => {
+                finalData += data;
+            }))
+            .then(stream => stream.on( 'end', () => {
 
-        const downloadFile = (url) => {
-  return new Promise((resolve, reject) => {
-    https.get(url,
-      (res) => {
-        if(res.headers.location) {
-          return downloadFile(res.headers.location).then(stream => resolve(stream));
-        }
-        resolve(res);
-      });
-  });
-};
-
-downloadFile('https://drive.google.com/uc?export=download&id=1tCm9Xb4mok4Egy2WjGqdYYkrGia0eh7X')
-.then(stream => stream.pipe(process.stdout))
-
-        /*fs.readdir(__dirname + "/" + dirPath, function(err, filenames) {
-            if (err) {
-                console.error(err);
-                return;
-            }
-            console.log(filenames);
-            filenames.forEach(function(filename) {
-                fs.readFile(__dirname + "/" + dirPath + filename, 'utf-8', function(err, content) {
+                fs.writeFile(bundleLocation, '\n\r' + finalData,  {'flag':'a'},  function(err) {
                     if (err) {
-                        console.error(err);
-                        return;
+                        return console.error(err);
+                    } else {
+                        console.log('web data received and appended');
                     }
-                    // onFileContent(filename, content);
-                    console.log(filename, content);
-                    fs.createWriteStream(__dirname + "/" + dirPath + 'bundle.css');
                 });
-            });
-        });*/
-
+            })
+            );
+        });
 
     }
    
